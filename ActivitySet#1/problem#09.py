6c802d76c7de4eacae75473674b0fc070c0a288e
exvynai <vir200319@gmail.com>
2022-05-07 00:49:23 +0530

    p9 done

--- ActivitySet#1/problem#09.py
+++ ActivitySet#1/problem#09.py
@@ -1,3 +1,14 @@
 # Lists
 
 filename = "dataset/romeo.txt"
+fhandle = open(filename,'r')
+all_words = []
+for line in fhandle:
+    line = line.rstrip()
+    words = line.split()
+    for word in words:
+        if all_words.count(word) == 0:
+            all_words.append(word)
+
+all_words.sort()
+print(all_words)
