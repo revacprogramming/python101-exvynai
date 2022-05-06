dce12aed82548345f2cbc553a10b8551ff4dcf57
exvynai <vir200319@gmail.com>
2022-05-07 00:48:34 +0530

    p8 done

--- ActivitySet#1/problem#08.py
+++ ActivitySet#1/problem#08.py
@@ -1,3 +1,19 @@
 # Files
 
+def getValue(line):
+    ipos = line.find('0')
+    epos = line.find('\n')
+    return float(line[ipos:epos])
+
+
 filename = "dataset/mbox-short.txt"
+fhandle = open(filename, 'r')
+count = 0
+total = 0
+for line in fhandle:
+    if line.startswith("X-DSPAM-Confidence:"):
+        count =  count + 1
+        total = total + getValue(line)
+
+print("Average spam confidence:", total/count)
+        
