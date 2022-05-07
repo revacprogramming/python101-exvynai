3af116ac2d550185250dab6f2fddd59ef08b016b
exvynai <vir200319@gmail.com>
2022-05-07 17:07:40 +0530

    p11 done

--- ActivitySet#1/problem#11.py
+++ ActivitySet#1/problem#11.py
@@ -1,3 +1,24 @@
 # Tuples
 
 filename = "dataset/mbox-short.txt"
+
+stats = dict()
+fhandle = open(filename, 'r')
+for line in fhandle:
+    line = line.rstrip()
+    words = line.split()
+    if "From" in words:
+        time = words[5]
+        hour = time[:2]
+        if hour in stats:
+            stats[hour] = stats.get(hour, 0) + 1
+        else:
+            stats.update({hour:1})
+
+counts = list()
+for k, v in stats.items():
+    counts.append((k, v))
+
+for (k, v) in sorted(counts):
+    print(k, v)
+
