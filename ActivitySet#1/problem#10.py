21f131d1c8d62a010dd4594ba95e924102a64887
exvynai <vir200319@gmail.com>
2022-05-07 16:27:39 +0530

    p10 done

--- ActivitySet#1/problem#10.py
+++ ActivitySet#1/problem#10.py
@@ -1,3 +1,26 @@
 # Dictionaries
 
 filename = "dataset/mbox-short.txt"
+
+fhandle = open(filename, 'r')
+stats = dict()
+for line in fhandle:
+    line = line.rstrip()
+    words = line.split()
+    if "From" in words:
+        name = words[1]
+        if name in stats:
+            stats[name] = stats.get(name, 0) + 1
+        else:
+            stats.update({name:1})
+
+temp = stats.values()
+max_value = max(temp)
+
+for k, v in stats.items():
+    if v == max_value:
+        print(k, v)
+
+
+
+
