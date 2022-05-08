e347caf8d5ee6492d51297aa11d5dcd466903484
exvynai <vir200319@gmail.com>
2022-05-08 13:15:34 +0530

    p12 and its data set

--- ActivitySet#1/problem#12.py
+++ ActivitySet#1/problem#12.py
@@ -1,2 +1,20 @@
 # Regular Expressions
 # https://www.py4e.com/lessons/regex
+#Note
+#the website did not accept this, need to fix code
+
+import re
+
+fhandle = open("dataset/regex_sum_1543883.txt", "r")
+total = 0
+for line in fhandle:
+    line = line.rstrip()
+    number = re.findall('[0-9]+', line)
+    if len(number) != 0:
+        for num in number:
+            total = total + int(num)
+     
+print(total)    
+    
+
+
