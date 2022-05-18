0454ccd9998221ad4e118083b672b3673607d1d4
exvynai <vir200319@gmail.com>
2022-05-18 10:28:44 +0530

    p14 done

--- ActivitySet#1/problem#14.py
+++ ActivitySet#1/problem#14.py
@@ -1,2 +1,28 @@
 # Using Web Services
 # https://www.py4e.com/lessons/servces
+
+import urllib.request, urllib.parse, urllib.error
+from urllib.request import urlopen
+import xml.etree.ElementTree as ET
+import ssl
+
+# Ignore SSL certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+url = input("Enter URL:" )
+xml = urlopen(url, context=ctx)
+print("retrieving", url)
+
+data = xml.read()
+print('Retrieved', len(data), 'characters')
+tree = ET.fromstring(data)
+results = tree.findall("comments/comment")
+
+total = 0
+for i in results:
+    number = int(i.find('count').text)
+    total = total + number
+
+print(total)
