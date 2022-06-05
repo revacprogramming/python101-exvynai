5a8b4d9591938a65724b0f98a488899a2cb2b803
exvynai <vir200319@gmail.com>
2022-06-05 22:48:25 +0530

    set2p3 done

--- ActivitySet#2/problem#3.py
+++ ActivitySet#2/problem#3.py
@@ -1,12 +1,12 @@
-
-
 def get_cs():
     """get string input"""
-
+    cs = input("Enter a string: ")
+    return cs
 
 def cs_to_lot(cs):
     """convert connected string to list of strings"""
-
+    lot = cs.split()
+    return lot
 
 def main():
     cs = get_cs()
